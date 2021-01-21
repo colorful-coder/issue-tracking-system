9872377ce0a4ec681ad80c9195f799e420550edd
Thet Minn Htun <thetminnhtun92@gmail.com>
2021-01-21 21:09:01 +0630

    WIP

---
+++ resources/views/position/index.blade.php
@@ -0,0 +1,91 @@
+<x-app-layout>
+    <div class="container mx-auto px-6 py-12">
+        <div class="card">
+
+            <div class="card-header">
+                <h3 class="font-bold">Developers List</h3>
+            </div>
+
+            <div class="card-body">
+                <a href="{{ route('developer.create') }}" class="btn btn-primary">Create</a>
+
+                <!-- Developers List -->
+                <div class="mt-6 shadow overflow-hidden border-b border-gray-200 rounded-lg">
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead class="bg-gray-50">
+                            <tr>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Name
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Position
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Team
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Role
+                                </th>
+                                <th scope="col" class="relative px-6 py-3">
+                                    <span class="sr-only">Edit</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+
+                            @forelse ($developers as $developer)
+                                
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="flex items-center">
+                                        <div class="flex-shrink-0 h-10 w-10">
+                                            <img class="h-10 w-10 rounded-full"
+                                                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+                                                alt="">
+                                        </div>
+                                        <div class="ml-4">
+                                            <div class="text-sm font-medium text-gray-900">
+                                                {{ $developer->name }}
+                                            </div>
+                                            <div class="text-sm text-gray-500">
+                                                {{ $developer->email }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    Junior
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    Team A
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    Admin
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                </td>
+                            </tr>
+                            @empty
+                                <tr>
+                                    <td class="p-6 text-gray-500 text-sm">No Developer.</td>
+                                </tr>     
+                            @endforelse
+
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="pagination mt-6">
+                    {{ $developers->links() }}
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DeveloperController;
 use App\Http\Controllers\IssueController;
+use App\Http\Controllers\PositionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,3 +16,7 @@ Route::get('/dashboard', function () {
 require __DIR__.'/auth.php';
 
 Route::resource('issue', IssueController::class)->middleware(['auth']);
+Route::resource('developer', DeveloperController::class)->middleware(['auth']);
+Route::resource('role', DeveloperController::class)->middleware(['auth']);
+Route::resource('category', IssueController::class)->middleware(['auth']);
+Route::resource('project', IssueController::class)->middleware(['auth']);
